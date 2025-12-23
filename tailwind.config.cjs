86c9880c0776af2a519b8c0326e701ab033b714f
vinomathiyan90 <vinomathiyan90@gmail.com>
2025-12-23 09:30:05 +0530

    Add files via upload

---
+++ tailwind.config.cjs
@@ -0,0 +1,12 @@
+const withMT = require("@material-tailwind/react/utils/withMT");
+
+module.exports = withMT({
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+});
